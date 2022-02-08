2c36a90b39c2a9a21ccecd5adb7d3b3f20b4cbc4
dmitriyphp <99209882+dmitriyphp@users.noreply.github.com>
2022-02-08 14:55:56 +0200

    Initial commit

---
+++ simple.php
@@ -0,0 +1,31 @@
+<?php
+
+const N = 100;
+
+function getSimpleNumbers(): Generator
+{
+    for ($i = 3; $i <= N; $i++)
+    {
+        $is_simple = true;
+        for ($j = 2; $j < $i; $j++)
+        {
+            if (fmod($i, $j) == 0)
+            {
+                $is_simple = false;
+            }
+        }
+        if ($is_simple)
+        {
+            yield $i;
+        }
+    }
+}
+
+function showSimpleNumbers() {
+    foreach (getSimpleNumbers() as $number) {
+        echo $number . "\n";
+    }
+}
+
+showSimpleNumbers();
+
